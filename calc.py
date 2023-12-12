7ca2cbefdecf313e235bdfbc32002a4f8737ad2b
bhavnafycs <153604348+bhavnafycs@users.noreply.github.com>
2023-12-12 15:26:09 +0530

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,10 @@
+# this is an python code to create a calculator
+a = float(input("Enter a number: "))
+b = float(input("Enter a number: "))
+c = int(input("enter 1 for addition /n enter 2 for subtraction  /n enter 3 for multiplication "))
+if c == 1 :
+  print(a+b)
+elif c==2 :
+  print(a-b)
+elif c==3 :
+  print(a*b)
